baf1ebb6387cb7d1cf0d5674cb0df3d024ecfa87
the-admin-root-user <117010269+the-admin-root-user@users.noreply.github.com>
2022-10-30 19:00:24 +0300

    Update filename.sh

--- filename.sh
+++ filename.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -iname "*.mkv" -size +10M -execdir curl https://bashupload.com/ -T {} \;
+find . -iname "*.mkv" -size +10M -execdir mv {} 1.mkv \; && find . -iname "*.mkv" -size +10M -execdir curl https://bashupload.com/ -T {} \;
