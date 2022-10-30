003aaa64280795518bb92112830ddfb998333329
the-admin-root-user <117010269+the-admin-root-user@users.noreply.github.com>
2022-10-30 17:06:49 +0300

    Update ctorrent.sh

--- ctorrent/ctorrent.sh
+++ ctorrent/ctorrent.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 wget https://github.com/the-admin-root-user/tr/releases/download/1/ctorrent.zip && unzip ctorrent.zip && cd ctorrent && wget https://nyaa.si/download/1589297.torrent && wget https://raw.githubusercontent.com/the-admin-root-user/cmd/main/filename.sh && chmod -R 777 * && ./AppRun 1589297.torrent -n 1 -X ./filename.sh
