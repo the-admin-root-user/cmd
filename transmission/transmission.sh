fa2ffd0de5cfd848807e7d1754332501719ead1b
the-admin-root-user <117010269+the-admin-root-user@users.noreply.github.com>
2022-10-30 17:04:32 +0300

    Update transmission.sh

--- transmission/transmission.sh
+++ transmission/transmission.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 wget https://github.com/the-admin-root-user/tr/releases/download/1/transmission.zip && unzip transmission.zip  && cd transmission && wget https://nyaa.si/download/1589297.torrent && wget https://raw.githubusercontent.com/the-admin-root-user/cmd/main/filename.sh && chmod -R 777 * && ./AppRun 1589297.torrent -w . -f ./filename.sh
